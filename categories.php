79e3307913ac43eb672c1ddc5868b58f7edd76f3
Thomas <thomas@iseabloom.com>
2017-04-29 17:26:38 +0200

    fragmentation home page

---
+++ categories.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 29/04/2017
+ * Time: 17:20
+ */
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once "vue/header.html";
+include_once "vue/home.html";
+include_once "vue/footer.html";
+
+
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 29/04/2017
+ * Time: 17:11
+ */
