32331a4580a339a77730bd89b2298582cf1c951a
Thomas <thomas@iseabloom.com>
2017-05-06 18:01:15 +0200

    retouches mobile #1 + script touch screen

---
+++ vue/js/main.js
@@ -0,0 +1,17 @@
+$('a.col').on("touchstart", function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    }
+    else {
+        link.addClass('hover');
+        $('a.col').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
+});
+
+/**
+ * Created by thomas on 06/05/2017.
+ */
