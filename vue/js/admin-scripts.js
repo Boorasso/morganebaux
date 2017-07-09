cd54adc91bfd07a8ed171885fa152e98da718ea1
thomas <thomas@iseabloom.com>
2017-07-09 19:58:40 +0200

    arrows up and down

--- vue/js/admin-scripts.js
+++ vue/js/admin-scripts.js
@@ -44,23 +44,53 @@ function getProject() {
 
     //Get images
     var images = $('#mainsection').find("div[class^=col-]");
-    console.log(images);
 
 }
 
 
-function readURL(input, targetDiv) {
+function readURL(input, targetDiv, targetLink) {
     console.log("function readURL");
+
+    targetLink = targetLink || 0;
+
+    console.log(targetLink);
 
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            console.log(targetDiv);
             targetDiv.css('background-image', 'url('+e.target.result +')');
+            if (targetLink != 0) {
+                targetLink.attr("href", e.target.result);
+            }
         }
 
         reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function  toggleUpAndDownButtons(element) {
+
+    var allImages = $('#mainsection').find("div[class^=col-]"),
+        firstElement = allImages[0],
+        lastElement = allImages[((allImages.length)-1)];
+
+    console.log(element);
+    console.log(lastElement);
+    console.log(element.is(lastElement));
+
+    if (element.is(firstElement)) {
+        element.find('.image-up').addClass("hidden");
+    }
+    else {
+        element.find('.image-up').removeClass("hidden");
+    }
+
+    if (element.is(lastElement)) {
+        element.find('.image-down').addClass("hidden");
+    }
+    else {
+        element.find('.image-down').removeClass("hidden");
     }
 }
 
@@ -69,22 +99,30 @@ function readURL(input, targetDiv) {
 
 
 
-
 $(document).ready(function() {
 
+    //PREVENT BUTTONS DEFAULT
+    $('button:not([type=submit])').click(function(e) {e.preventDefault();});
+
+    //TOGGLE ALT FIELD
     $('.tag_button').click(function() {
         $('#alt_img').toggleClass("hidden");
     });
 
+    //TOGGLE SMALL BUTTONS
     $('.hide-buttons').click(function(e) {
         e.preventDefault();
         $('.admin-icons').toggleClass('hidden');
     });
 
-    //Preview
-    $('.edit-description').click(function() {
-        console.log("Preview");
 
+
+    /*--------------------------------------------
+                        PREVIEW
+    --------------------------------------------*/
+
+    //PREVIEW BUTTON
+    $('.edit-description').click(function() {
         var buttonValue = $(this).html(),
         nomProjet = $('#nom_projet').val(),
         colorTitle = $('#couleur_titre').val(),
@@ -93,11 +131,6 @@ $(document).ready(function() {
 
         if (buttonValue == "Preview") {
             $(this).html('Mode édition');
-
-            console.log(nomProjet + ' ' + colorTitle + ' ' + posteTitle + ' ' + descriptionText);
-
-            console.log($('#titreprojet').css('color'));
-
             $('#titreprojet').html(nomProjet);
             $('#titreprojet').css('color', "#"+colorTitle);
             $('#poste').html(posteTitle);
@@ -111,16 +144,43 @@ $(document).ready(function() {
         $('.project-description-preview').toggleClass('hidden');
     });
 
-    CKEDITOR.instances['text'].on('change', function() {
-        console.log(CKEDITOR.instances['text'].getData());
-    });
-
-    getProject();
-
+    //IMAGE PREVIEW
     $(".image-input").change(function(){
         var target = $(this).parent().parent();
-        readURL(this, target);
+        var targetLink = $(this).parent().parent().find(".colcontent");
+        readURL(this, target, targetLink);
     });
+
+    //IMAGE UP
+    $(document).find('.image-up').click(function(e) {
+        e.preventDefault();
+
+        var movingUP = $(this).parent().parent(),
+            movingDOWN = movingUP.prev();
+
+        movingDOWN.before(movingUP);
+
+        toggleUpAndDownButtons(movingUP);
+        toggleUpAndDownButtons(movingDOWN);
+
+    });
+
+    //IMAGE DOWN
+    $(document).find('.image-down').click(function(e) {
+        e.preventDefault();
+
+        var movingDOWN = $(this).parent().parent(),
+            movingUP = movingDOWN.next();
+
+        movingDOWN.before(movingUP);
+
+        toggleUpAndDownButtons(movingUP);
+        toggleUpAndDownButtons(movingDOWN);
+
+    });
+
+    //DATA GETTER (objet projet)
+    getProject();
 
 
 });
