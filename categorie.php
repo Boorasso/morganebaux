a232c2aa12dc0c3a4ffa06c183ef948f1c462de1
Thomas Bourasseau <tom.bourasseau@gmail.com>
2017-12-06 13:03:05 +0100

    improve category filtering

--- categorie.php
+++ categorie.php
@@ -4,8 +4,8 @@ require_once "model/conf.php";
 
 
 //1- Tester la variable reçu en GET
-//Tester la variable récupérée en GET !
 if (isset($_GET['ref'])){
+    //TODO filtrer la variable récupérée en GET !
     $ref_projet = $_GET['ref'];
 }
 else {
@@ -15,31 +15,49 @@ else {
 //2- Connexion à la base projets
 include_once "model/connexion_bdd.php";
 
-//3- Sortir les projets de la catégorie choisie
-if ($ref_projet == "all") {
-    $query_projets = $bdd->prepare('SELECT projets.id_projet AS id_projet, projets.nom_projet AS nom_projet, images.url_img AS url_img
-                                FROM projets
-                                INNER JOIN images
-                                ON projets.id_projet = images.idprojet
-                                WHERE images.pos_img = 1
-                                ORDER BY projets.date_projet DESC;');
-    $query_projets->execute(array());
-    $projets_cat = $query_projets->fetchAll();
-    $query_projets->closeCursor();
+//3- Vérifier que la catégorie demandée existe dans la BDD
+// Récupération des id de catégogires en base
+$query_categories = $bdd->prepare('SELECT categories.id_cat
+                                FROM categories
+                                ORDER BY categories.id_cat ASC;');
+$query_categories->execute(array());
+$categories_id = $query_categories->fetchAll();
+$query_categories->closeCursor();
+// Validation de l'id reçu en get
+if (in_array($ref_projet, $categories_id)) {
+  $category_exists = true;
 }
 else {
+  $category_exists = false;
+}
+
+//4- Sortir les projets de la catégorie choisie
+if ($category_exists) {
     $query_projets = $bdd->prepare('SELECT projets.id_projet AS id_projet, projets.nom_projet AS nom_projet, images.url_img AS url_img
-                                FROM projets
-                                INNER JOIN images
-                                ON projets.id_projet = images.idprojet
-                                WHERE images.pos_img = 1 AND projets.id_cat = ?
-                                ORDER BY projets.date_projet DESC;');
+                                    FROM projets
+                                    INNER JOIN images
+                                    ON projets.id_projet = images.idprojet
+                                    WHERE images.pos_img = 1 AND projets.id_cat = ?
+                                    ORDER BY projets.date_projet DESC;');
     $query_projets->execute(array($ref_projet));
     $projets_cat = $query_projets->fetchAll();
     $query_projets->closeCursor();
 }
+// Default to the "ALL" display
+else {
+    $query_projets = $bdd->prepare('SELECT projets.id_projet AS id_projet, projets.nom_projet AS nom_projet, images.url_img AS url_img
+                                    FROM projets
+                                    INNER JOIN images
+                                    ON projets.id_projet = images.idprojet
+                                    WHERE images.pos_img = 1
+                                    ORDER BY projets.date_projet DESC;');
+    $query_projets->execute(array());
+    $projets_cat = $query_projets->fetchAll();
+    $query_projets->closeCursor();
+}
 
-//4- Lancer le rendu des templates
+
+//5- Lancer le rendu des templates
 include_once "vue/header.tpl.php";
 include_once "vue/categorie.tpl.php";
 include_once "vue/footer.tpl.php";
