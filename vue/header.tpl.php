af72f5c7c0df51cd93c7fdda9e838b3fc494589f
Thomas Bourasseau <tom.bourasseau@gmail.com>
2017-12-08 18:54:51 +0100

    ajout favicon

--- vue/header.tpl.php
+++ vue/header.tpl.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width initial-scale=1">
-    <link rel="icon" type="image/png" href="vue/img/morgane logo.svg" />
+    <link rel="icon" type="image/png" href="vue/img/morgane logo.png" />
     <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="vue/css/reset.css">
