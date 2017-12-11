9e0dbb1dc83a932e2ede299503fdb25feceeabd2
Thomas Bourasseau <tom.bourasseau@gmail.com>
2017-12-11 16:35:21 +0100

    mise à jour projet + images + essai page actualite CV

--- model/conf.php
+++ model/conf.php
@@ -1,3 +1,3 @@
 <?php
 
-$login = true;
+$login = false;
---
+++ test-actu-cv.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "model/conf.php";
+
+//Variable titre
+$page_title = "Test Actu CV";
+
+include_once "vue/header.tpl.php";
+include_once "vue/test-actu-cv.html";
+include_once "vue/footer.tpl.php";
