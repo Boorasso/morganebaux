131a2510fc1fb05e0b63cf9320421d9c3571c09e
Thomas Bourasseau <tom.bourasseau@gmail.com>
2018-02-02 12:36:40 +0100

    ajout custom js

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,18 +1,28 @@
+$( document ).ready(function() {
 
-//Touch hover fix
-$('a.col').on("touchend", function (e) {
-    'use strict'; //satisfy code inspectors
-    var link = $(this).last(); //preselect the link
+  //Déclenchement de l'effet de hover en tactile
+  $('a.col').on("touchend", function (e) {
+    var link = $(this).last();
     if (link.hasClass('hover')) {
-        return true;
+      return true;
     }
     else {
-        link.addClass('hover');
-        $('a.col').not(this).removeClass('hover');
-        e.preventDefault();
-        return false; //extra, and to make sure the function has consistent return points
+      link.addClass('hover');
+      $('a.col').not(this).removeClass('hover');
+      e.preventDefault();
+      return false;
     }
+  });
+
+  //Petit message en console pour la 3WA
+  document.onmousedown = function(event) {
+    if (event.which == 3) { /*Click droit*/
+        console.log("Bonjour 3WA !");
+    }
+  }
+
 });
+
 
 /**
  * Created by thomas on 06/05/2017.
